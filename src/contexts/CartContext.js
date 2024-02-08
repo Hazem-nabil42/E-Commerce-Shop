b87f71427b309ae4e8ee0ead01ef4d801fa67a5a
unknown <hazem-nabil42>
2024-02-08 23:58:56 +0200

    first commit

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,126 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+// create context
+export const CartContext = createContext();
+
+const CartProvider = ({children}) => {
+  // cart state
+  const [ cart, setCart ] = useState([]);
+  // item amount state
+  const [itemAmount, setItemAmount] = useState(0);
+  // total price
+  const [total, setTotal] = useState(0);
+
+  //update item amount
+  useEffect(() => {
+    if (cart) {
+      const amount = cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.amount;
+      }, 0)
+      setItemAmount(amount);
+    }
+  }, [cart])
+
+  // total price 
+  useEffect(() => {
+    if (cart) {
+      const amount = cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.price * currentItem.amount;
+      }, 0)
+      setTotal(amount);
+    }
+  }, [cart])
+
+  const addToCart = (product, id) => {
+    // info about the item to be desplayed in the cart
+    const newItem = {...product, amount: 1}
+    // check if the item is already in the cart
+    const cartItem = cart.find(item => {
+      return item.id === id 
+    });
+    // if cartItem true do this
+    if (cartItem) {
+
+      const newCart = [...cart].map(item => {
+        if (item.id === id) {
+          return {...item, amount: cartItem.amount + 1};
+        }else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+    // console.log(cartItem);
+    // console.log(`item ${product.title} added To Cart Successfuly`);
+  };
+  // console.log(cart);
+
+
+  // remove from cart
+  const removeFromCart = (id) => {
+    const newCart = cart.filter(item => {
+      return item.id !== id
+    });
+    setCart(newCart)
+  }
+  //clear the cart
+  const clearCart = () => {
+    setCart([])
+  }
+
+  // increase amount
+  const increaseAmount = (id) => {
+    const cartItem = cart.find(item => {
+
+      addToCart(item, id);
+    })
+  }
+
+  // decrease the amount
+  const decreaseAmount = (id) => {
+    // check if the item id already exist
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    });
+    // if the item exist do this decrease the amount
+    if (cartItem) {
+      const newCart = cart.map(item => {
+        if (item.id === id) {
+          return {...item, amount: cartItem.amount - 1};
+        }else {
+          return item;
+        }
+      });
+      setCart(newCart);
+      } 
+        
+      if (cartItem.amount < 2) {
+          removeFromCart(id);
+      }
+      
+    }
+
+
+
+
+
+
+  return (
+    <CartContext.Provider
+      value={ { 
+        cart,
+        addToCart, 
+        removeFromCart, 
+        clearCart, 
+        increaseAmount, 
+        decreaseAmount,
+        itemAmount,
+        total } }>
+      {children}
+    </CartContext.Provider>
+    );
 };
 
-export default CartContext;
+export default CartProvider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+//import product context
+import { ProductContext } from '../contexts/ProductContext'
+
+// import components
+import Product from '../components/Product'
+
+// import Hero
+import Hero from '../components/Hero'
 
 const Home = () => {
-  return <div>Homepage</div>;
+  // get products from product context
+  const { products }  = useContext(ProductContext);
+
+  //get only men & women clothing category
+  const filteredProducts = products.filter(item => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  // console.log(filteredProducts);
+  return (
+    <div>
+      <Hero />
+      <section className='py-16'>
+        <div className='container mx-auto'>
+          <div className='grid grid-cols-1 md:grid-cols-2 
+          lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm
+          mx-auto md:max-w-none md:mx-0 xl:mx-20'>
+            {filteredProducts.map(product => {
+              return(
+                // passing each filtered product to the Product component to handle its presentation in the Web app
+                <Product product={product} key={product.id}/>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
